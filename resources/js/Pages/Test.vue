7e49f1fdf7ba8420a93ccfc0cbcd6f709a828d51
Nikolay <NikolayUlyanov@yandex.ru>
2023-09-29 12:24:00 +0300

    Install Swiper

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,51 @@
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        const onSwiper = (swiper) => {
+            console.log(swiper);
+        };
+        const onSlideChange = () => {
+            console.log('slide change');
+        };
+        return {
+            onSwiper,
+            onSlideChange,
+        };
+    },
+};
+</script>
+<template>
+    <swiper
+        :slides-per-view="3"
+        :space-between="1"
+        @swiper="onSwiper"
+        @slideChange="onSlideChange"
+    >
+        <swiper-slide>
+            <img src="/storage/images/001.jpg" alt="">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="/storage/images/002.jpg" alt="">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="/storage/images/003.jpg" alt="">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="/storage/images/004.jpg" alt="">
+        </swiper-slide>
+        <swiper-slide>
+            <img src="/storage/images/005.jpg" alt="">
+        </swiper-slide>
+    </swiper>
+</template>
+
