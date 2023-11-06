6d249f5c7cb57960a6ad15fe5547f46188af32b5
Nikolay <NikolayUlyanov@yandex.ru>
2023-11-06 21:46:57 +0300

    wip

--- resources/js/Pages/Admin/Pages/Create.vue
+++ resources/js/Pages/Admin/Pages/Create.vue
@@ -3,15 +3,25 @@ import {useForm} from "@inertiajs/vue3";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import InputError from "@/Components/InputError.vue";
 import LabelInput from "@/Pages/Admin/Partials/LabelInput.vue";
+import {ref} from "vue";
 
 let props = defineProps({page: Object,});
 
 let form = useForm({
     title: '',
     slug: '',
+    published_at: null,
 });
 
+let publish = ref(true);
+
 let submit = () => {
+    if(publish.value){
+        form.published_at = new Date().toISOString().replace("T"," ").substring(0, 19);
+    }else{
+        form.published_at = null;
+    }
+
     form.post(route('pages.store'));
 };
 </script>
@@ -31,9 +41,15 @@ export default {layout: AdminLayout}
         <form @submit.prevent="submit" class="">
             <LabelInput input-class="w-[400px]" v-model="form.title" :error-msg="form.errors.title" name="pageTitle">Название страницы</LabelInput>
             <LabelInput input-class="w-[400px]" v-model="form.slug" :error-msg="form.errors.slug" name="pageSlug">slug</LabelInput>
+            <div @click="publish = ! publish"
+                 class="w-[150px] text-center rounded px-1 text-white cursor-pointer"
+                :class="publish ? 'bg-green-600' : 'bg-gray-600'"
+            >
+                {{ publish ? 'опубликована' : 'не опубликована' }}
+            </div>
 
-            <button class="btn btn-blue" :disabled="form.processing">
-                Опубликовать
+            <button class="mt-4 btn btn-blue" :disabled="form.processing">
+                Создать
             </button>
         </form>
     </div>
